e33d188036fbb83559b0d73b36a0a01b43d1418b
LongNQ <long.nq.hust@gmail.com>
2024-02-28 12:01:01 +0700

    communicate between serivces

--- inventory-service/src/main/java/com/gonl/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/gonl/inventoryservice/controller/InventoryController.java
@@ -1,9 +1,12 @@
 package com.gonl.inventoryservice.controller;
 
+import com.gonl.inventoryservice.dto.InventoryResponse;
 import com.gonl.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,9 +14,9 @@ import org.springframework.web.bind.annotation.*;
 public class InventoryController {
     private final InventoryService inventoryService;
 
-    @GetMapping("/{sku-code}")
+    @GetMapping()
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode){
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode){
         return inventoryService.isInStock(skuCode);
     }
 }
--- order-service/src/main/java/com/gonl/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/gonl/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.gonl.orderservice.service;
 
+import com.gonl.orderservice.dto.InventoryResponse;
 import com.gonl.orderservice.dto.OrderLineItemDto;
 import com.gonl.orderservice.dto.OrderRequest;
 import com.gonl.orderservice.model.Order;
@@ -8,7 +9,9 @@ import com.gonl.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,12 +20,30 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
-    public void placeOrder(OrderRequest orderRequest){
+    private final WebClient webClient;
+
+    public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
-        List<OrderLineItems> orderLineItems =  orderRequest.getOrderLineItemDtoList().stream().map(this::mapToDto).toList();
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemDtoList().stream().map(this::mapToDto).toList();
         order.setOrderLineItemList(orderLineItems);
-        orderRepository.save(order);
+
+        List<String> skuCodes = order.getOrderLineItemList().stream().map(OrderLineItems::getSkuCode).toList();
+        // Call Inventory service and place order if product is in stock
+        InventoryResponse[] inventoryResponses = webClient.get().uri("http://localhost:8082/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        boolean allProductsInStock =  Arrays.stream(inventoryResponses).allMatch(InventoryResponse::isInStock);
+
+        if (allProductsInStock) {
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not in stock");
+        }
+
 
     }
 
